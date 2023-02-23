0872d4b3da7a6a6c5edca584023e8d653b2256bb
shinafifloo <effati.afshin@gmail.com>
2023-02-22 22:55:10 -0500

    p8

--- jobs/app.py
+++ jobs/app.py
@@ -51,5 +51,11 @@ def employer(employer_id):
 @app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
 def review(employer_id):
     if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
         return redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
